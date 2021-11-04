4bc0c6a639dd569b71ae10930c602df0615ea848
briandgomez <briandgomez98@gmail.com>
2021-11-04 14:03:03 -0400

    return notes.html

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const PORT = 3001;
+const PORT = 3005;
 
 //Return notes.html when using '/notes' in the route
-app.get('/api/notes', (req, res) => {
-    res.sendFile(path.join, (__dirname, 'notes.html'));
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, 'notes.html'));
 });
 
 app.listen(PORT, () => {
