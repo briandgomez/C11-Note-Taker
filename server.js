76abec061cea2bc26add6a5110022387212169de
briandgomez <briandgomez98@gmail.com>
2021-11-05 18:37:55 -0400

    return db.json

--- server.js
+++ server.js
@@ -1,8 +1,22 @@
+const fs = require('fs');
 const express = require('express');
 const app = express();
 const path = require('path');
 const PORT = 3005;
+const [returnedNotes] = require('./db/db.json');
 
+/*API Routes*/
+//Return notes as json
+app.get('/api/notes', (req,res) => {
+    res.json(returnedNotes);
+});
+
+//
+app.post('/api/notes',(re,res) => {
+
+});
+
+/* HTML Routes*/
 //Return notes.html
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, './public/notes.html'));
@@ -11,7 +25,8 @@ app.get('/notes', (req, res) => {
 //Return index.html
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, './public/index.html'));
-})
+});
+
 app.listen(PORT, () => {
     console.log(`App listening on PORT ${PORT}`);
 });
