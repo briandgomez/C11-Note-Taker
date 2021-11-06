86e3ff31ae3e6e4323321c7f046d72b02291871c
briandgomez <briandgomez98@gmail.com>
2021-11-06 11:46:51 -0400

    delete note

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const app = express();
 const path = require('path');
 const PORT = 3005;
 const returnedNotes = require('./db/db.json');
+const { match } = require('assert');
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -12,24 +13,35 @@ app.use(express.static('public'));
 
 /*API Routes*/
 //Return notes as json
-app.get('/api/notes', (req,res) => {
+app.get('/api/notes', (req, res) => {
     res.json(returnedNotes);
 });
 
 //Saves note
-app.post('/api/notes',(req,res) => {
-    console.log(req.body);
+app.post('/api/notes', (req, res) => {
+    //onsole.log(req.body);
     req.body.id = uniqid();
     returnedNotes.push(req.body);
-    fs.writeFile('./db/db.json', JSON.stringify(returnedNotes), (err, data)=> {
-        if(err) throw err;
+    fs.writeFile('./db/db.json', JSON.stringify(returnedNotes), (err, data) => {
+        if (err) throw err;
     });
     res.json(returnedNotes);
 });
 
-//
-app.delete('/api/notes/:id', (req,res) => {
-    req.params.id
+//Delete specific note
+app.delete('/api/notes/:id', (req, res) => {
+    const id = req.params.id;
+    const foundNote = returnedNotes.find(function (note) {
+        if (note.id === id) {
+            return note
+        }
+    });
+    const index = returnedNotes.indexOf(foundNote);
+    returnedNotes.splice(index,1);
+    fs.writeFile('./db/db.json', JSON.stringify(returnedNotes), (err, data) => {
+        if (err) throw err;
+    });
+    res.send('Note successfully deleted');
 });
 
 /* HTML Routes*/
